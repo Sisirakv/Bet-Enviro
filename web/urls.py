55ecd3513ab85b625f7f9a0be2597ca5ba3950f0
Sisirakv <sisirakv1998@gmail.com>
2022-07-12 16:57:58 +0530

    service page 3.3 to 3.10

--- web/urls.py
+++ web/urls.py
@@ -14,5 +14,65 @@ urlpatterns = [
     path("About us/",views.about_us,name="About us"),
 
 
+    path('Water QT & diag', views.water_QT_diag,name='Water QT & diag'),
+    path('Water QT',views.water_QT,name="Water QT"),
+    path('Waste Water QT',views.waste_water,name='Waste Water QT'),
+
+
+    path('Water Treatment',views.Water_treat,name='Water Treatment'),
+    path('Water Filter',views.water_filter,name='Water Filter'),
+    path('Sand Filter',views.sand_filter,name='Sand Filter'),
+    path('Carbon Filter',views.carbon_filter,name='Carbon Filter'),
+
+
+    path('Commercials and industrial WT',views.commercial_ind,name="Commercials and industrial WT"),
+    path('Rverse OS',views.reverse_OS,name="Rverse OS"),
+    path('Ultra Filtertion',views.ultra_filtration,name="Ultra Filtertion"),
+    path('Water Demineralize',views.water_demineralize,name="Water Demineralize"),
+    path('Ozonation',views.ozonation,name="Ozonation"),
+    path('Packaged Drinking/',views.packaged_drinking,name="Packaged Drinking"),
+    path('Carbonated Soft Drink/',views.carbonated_softdrink,name="Carbonated Soft Drink"),
+    path('Pet Bottle BM/',views.pet_bottle,name="Pet Bottle BM"),
+
+    path('Waste Water Treatment',views.waste_WT,name="Waste Water Treatment"),
+    path('STP',views.stp,name="STP"),
+    path('MBBR',views.mbbr,name="MBBR"),
+    path('SBR',views.sbr,name="SBR"),
+    path('SAFF',views.saff,name="SAFF"),
+    path('BIO DIGESTER',views.bio_digester,name="BIO DIGESTER"),
+    path('ETP',views.etp,name="ETP"),
+    path('Grid Remover System',views.grid_remover,name="Grid Remover System"),
+    path('Oil Trap',views.oil_trap,name="Oil Trap"),
+
+    path('Food WM/',views.food_WM,name="Food WM"),
+    path('Biogas/',views.biogas,name="Biogas"),
+    path('Portable Biogas/',views.portable_bio,name="Portable Biogas"),
+    path('Underground Biogas/',views.underground_bio,name="Underground Biogas"),
+    path('Biocompost/',views.biocompost,name="Biocompost"),
+    path('Market Waste/',views.marker_waste,name="Market Waste"),
+
+    path('Solid WM/',views.solid_WM,name="Solid WM"),
+    path('INCINERATOR/',views.incinerator,name="INCINERATOR"),
+    path('Brick made/',views.brick_made,name="Brick made"),
+    path('SS/',views.ss,name="SS"),
+    path('MS/',views.ms,name="MS"),
+    path('Napkin Incinerator/',views.napkin,name="Napkin Incinerator"),
+    path('Napkin Vending/',views.napkin_vending,name="Napkin Vending"),
+    path('Plastic Recycling/',views.plastic,name="Plastic Recycling"),
+    
+    path('Solar Energy/',views.solar_energy,name="Solar Energy"),
+    path('On Grid/',views.on_grid,name="On Grid"),
+    path('Off Grid/',views.off_grid,name="Off Grid"),
+    path('Water Heater/',views.water_heater,name="Water Heater"),
+    path('Street light/',views.street_light,name="Street light"),
+
+    path('After Sale/',views.after_sale,name="After Sale"),
+    path('AMC/',views.amc,name="AMC"),
+    path('WT Chemical/',views.chemical_treatment,name="WT Chemical"),
+    path('WT Accessories/',views.wt_accessories,name="WT Accessories"),
+    path('Resetting/',views.resetting,name="Resetting"),
+
+
+
 
 ]
--- web/views.py
+++ web/views.py
@@ -53,11 +53,148 @@ def gallery(request):
     }
     return render(request,"web/gallery.html",context)
 
+def water_QT_diag(request):
+    return render(request,"web/wqt&diaganosis.html")
+
+def water_QT(request):
+    return render(request,"web/water-quality-test.html")
+
+def waste_water(request):
+    return render(request,"web/waste-water-treatment.html")
 
 
+def Water_treat(request):
+    return render(request,"web/water-treatment.html")
+
+def water_filter(request):
+    return render(request,"web/water-filter.html")
+
+def sand_filter(request):
+    return render(request,"web/sand-filter.html")
+
+def carbon_filter(request):
+    return render(request,"web/carbon-filter.html")
+
+def commercial_ind(request):
+    return render(request,"web/commercial and ind/commercial & ind.html")
+
+def reverse_OS(request):
+    return render(request,'web/commercial and ind/reverse os.html')
+
+def ultra_filtration(request):
+    return render(request,'web/commercial and ind/Ultra filtration.html')
+
+def water_demineralize(request):
+    return render(request,'web/commercial and ind/water demineralize.html')
+
+def ozonation(request):
+    return render(request,'web/commercial and ind/Ozonation.html')
+
+def packaged_drinking(request):
+    return render(request,'web/commercial and ind/packaged Drinking.html')
+
+def carbonated_softdrink(request):
+    return render(request,'web/commercial and ind/carbonated SD.html')
+
+def pet_bottle(request):
+    return render(request,'web/commercial and ind/pet bottle BM.html')
 
 
+def waste_WT(request):
+    return render(request,'web/waste water/waste WT.html')
 
+def stp(request):
+    return render(request,'web/waste water/STP.html')
 
+def mbbr(request):
+    return render(request,'web/waste water/MBBR.html')
 
+def sbr(request):
+    return render(request,'web/waste water/SBR.html')
 
+def saff(request):
+    return render(request,'web/waste water/SAFF.html')
+
+def bio_digester(request):
+    return render(request,'web/waste water/BIO DIGESTER.html')
+
+def etp(request):
+    return render(request,'web/waste water/ETP.html')
+
+def grid_remover(request):
+    return render(request,'web/waste water/grid remover.html')
+
+def oil_trap(request):
+    return render(request,'web/waste water/oil trap.html')
+
+def food_WM(request):
+    return render(request,'web/food waste/food WM.html')
+
+def biogas(request):
+    return render(request,'web/food waste/biogas.html')
+
+def portable_bio(request):
+    return render(request,'web/food waste/portable bio.html')
+
+def underground_bio(request):
+    return render(request,'web/food waste/underground bio.html')
+
+def biocompost(request):
+    return render(request,'web/food waste/biocompost.html')
+
+def marker_waste(request):
+    return render(request,'web/food waste/market waste.html')
+
+def solid_WM(request):
+    return render(request,'web/solid waste/solid WM.html')
+
+def incinerator(request):
+    return render(request,'web/solid waste/INCINERATOR.html')
+
+def brick_made(request):
+    return render(request,'web/solid waste/brick made.html')
+
+def ss(request):
+    return render(request,'web/solid waste/SS.html')
+
+def ms(request):
+    return render(request,'web/solid waste/MS.html')
+
+def napkin(request):
+    return render(request,'web/solid waste/napkin.html')
+
+def napkin_vending(request):
+    return render(request,'web/solid waste/napkin vending.html')
+
+def plastic(request):
+    return render(request,'web/solid waste/plastic.html')
+
+def solar_energy(request):
+    return render(request,'web/solar energy/solar energy.html')
+
+def on_grid(request):
+    return render(request,'web/solar energy/on grid.html')
+
+def off_grid(request):
+    return render(request,'web/solar energy/off grid.html')
+
+def water_heater(request):
+    return render(request,'web/solar energy/water heater.html')
+
+def street_light(request):
+    return render(request,'web/solar energy/street light.html')
+
+def after_sale(request):
+    return render(request,'web/after sale/after sale.html')
+
+def amc(request):
+    return render(request,'web/after sale/amc.html')
+
+def chemical_treatment(request):
+    return render(request,'web/after sale/WT chemical.html')
+
+def wt_accessories(request):
+    return render(request,'web/after sale/WT ass.html')
+
+def resetting(request):
+    return render(request,'web/after sale/resetting.html')

072fd2c4433e95d56fc3485e4f31d4e7e1e01d34
Sisirakv <sisirakv1998@gmail.com>
2022-07-13 16:47:08 +0530

    heroku1

--- betenviro/settings.py
+++ betenviro/settings.py
@@ -161,5 +161,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_TEMPLATE_PACK = 'Bootstrap5'

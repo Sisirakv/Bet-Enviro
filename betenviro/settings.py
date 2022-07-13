01229b80841c0a256f369da6d97c00203530f425
Sisirakv <sisirakv1998@gmail.com>
2022-07-13 16:49:26 +0530

    heroku1

--- betenviro/settings.py
+++ betenviro/settings.py
@@ -152,7 +152,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_URL = "/static/"
 STATIC_FILE_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = ((BASE_DIR / "static"),)
+STATICFILES_DIRS = ((BASE_DIR / "static"))
 STATIC_ROOT = BASE_DIR / "assets"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
